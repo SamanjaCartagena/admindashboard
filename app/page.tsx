03bd4ea21ea3ea7a7d739bab152b7265cd2398bf
Samanja Cartagena <c.samanja09@Pabs.local>
2024-08-12 11:48:01 -0400

    filtered posts added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import {Button} from "@/components/ui/button"
 import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/PostsTable";
+import { Newspaper, Folder, User, MessageCircle } from "lucide-react";
 export default function Home() {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-        <DashboardCard/>
+          <DashboardCard title='Posts'
+         count={100} icon={<Newspaper
+          className="text-slate-500" size={72} />}
+          />
+        <DashboardCard title='Categories'
+         count={12} icon={<Folder
+          className="text-slate-500" size={72} />}
+          />
+          <DashboardCard title='Users' 
+          count={750} icon={<User
+          className="text-slate-500" size={72} />}/>
+          <DashboardCard title='Comments' 
+          count={100} icon={<MessageCircle
+          className="text-slate-500" size={72} />}/>
       </div>
+      <PostsTable title='Latest Posts' limit={5}/>
     </>
   );
 }
